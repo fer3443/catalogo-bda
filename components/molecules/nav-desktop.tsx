48e0eaaffcd41ed6c1dbecc39ed80a1a4a40f826
Fernando Arroyo <fer3443@gmail.com>
2026-07-27 22:13:34 -0300

    feat(home): add location icon to header

--- components/molecules/nav-desktop.tsx
+++ components/molecules/nav-desktop.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
+import { ImLocation2 } from "react-icons/im";
 import { LocaleSwitcher } from "../localeswitcher"
 
 export const NavDesktop = () => {
   return (
     <nav className="bg-[#3f1b26] w-full py-2 px-6 flex justify-between items-center mb-4">
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-2 items-center">
+        <ImLocation2/>
         <Link
           className='text-base font-light text-secondary hover:text-white'
           href='https://www.google.com/maps/place/Bodega+Burbujas+de+Altura/@-26.0355294,-65.9807108,111m/data=!3m1!1e3!4m6!3m5!1s0x941f31cb8c95b503:0xfa032e61ca1bf6cc!8m2!3d-26.0354957!4d-65.9804077!16s%2Fg%2F11gxrcfj37?entry=ttu&g_ep=EgoyMDI2MDIxOC4wIKXMDSoASAFQAw%3D%3D'
